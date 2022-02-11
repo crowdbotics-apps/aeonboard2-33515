388d9de7b9ac2bb899cb34e6e3378dcdb7fbc128
Crowdbotics <team@crowdbotics.com>
2022-02-11 00:46:59 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class FlashCard(models.Model):
+    "Generated Model"
+    front = models.TextField()
+    back = models.TextField()
